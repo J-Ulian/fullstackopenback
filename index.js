9b21418cb8d5c5c7d0dd8765c6c87d74f5d2f38b
J Ulian <hudatov@gmail.com>
2020-06-28 16:18:32 +0300

    Phonebook database, step7

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.delete("/api/persons/:id", (request, response, next) => {
   // response.status(204).end();
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
   if (!body.name) {
     return response.status(400).json({
@@ -127,8 +127,11 @@ app.post("/api/persons", (request, response) => {
   });
   //persons = persons.concat(person);
   person.save().then(savedPerson => {
-    response.json(savedPerson.toJSON())
-  })
+      response.json(savedPerson.toJSON())
+    })
+    .catch(
+      error => next(error)
+    )
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -163,6 +166,10 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === "CastError") {
     return response.status(400).send({
       error: "malformatted id"
+    })
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({
+      error: error.message
     })
   }
 
